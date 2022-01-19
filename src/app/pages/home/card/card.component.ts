ed5334a80a317a034c8e02e688ea3d24115916f2
Melani Monje <flowneko220298m@gmail.com>
2022-01-19 15:34:58 -0300

    modifique cartas, banner e imagenes

--- src/app/pages/home/card/card.component.ts
+++ src/app/pages/home/card/card.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.css']
+  selector: 'app-card', //que es como llamarlo al componente en el html
+  templateUrl: './card.component.html', //que es el HTML del componeente
+  styleUrls: ['./card.component.css'] //los estilos del componente
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor() {}
+
+  @Input() titulo: string = ''; // el @Input recibe un valor del componente padre.
+  @Input() imagenDireccion: string = '';
+  @Input() ejemplo: any = '';
+  
 
   ngOnInit(): void {
   }
-
+  
 }
+
+
